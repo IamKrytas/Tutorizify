ba361ea675f1a1f15d1e1958cfaa663ef5dde0fb
IamKrytas <kedzierski.aleksander@gmail.com>
2025-07-05 00:29:50 +0200

    Implement interaction files for new structure

--- backend/app/services/interaction_service.py
+++ backend/app/services/interaction_service.py
@@ -1,5 +1,14 @@
-def favourite_teacher_service(teacher_id):
-    pass
+from app.models.interaction_model import get_reviews_model
 
 def get_reviews_service(teacher_id):
-    pass
+    result = get_reviews_model(teacher_id)
+    reviews = []
+    for review in result:
+        reviews.append({
+            "id": review["id"],
+            "rating": review["rating"],
+            "comment": review["comment"],
+            "createdAt": review["created_at"],
+            "username": review["username"]
+        })
+    return reviews
